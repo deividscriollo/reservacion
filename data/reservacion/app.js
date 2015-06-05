1dbc98943518ca5e578ef928862fa27358b0daff
Deivid <deividscriollo@gmail.com>
2015-06-05 16:53:05 -0500

    proceso en la nuve

--- data/reservacion/app.js
+++ data/reservacion/app.js
@@ -14,12 +14,81 @@ $(function(){
 	// selector 
 		$('#select_tipo_reser_museo').change(function(){
 			var valor_selecion=$(this).val();
-			alert(valor_selecion)
+            //institución
+			if (valor_selecion=="20150526092958556483663add0"
+                ||valor_selecion=="2015052609301055648372aaa83"
+                ||valor_selecion=="201505260930215564837d25753") {
+                console.log('test');
+                $('#obj_institucion').removeClass('hidden').addClass('zoomIn animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                  $(this).removeClass('zoomIn animated');
+                });
+            }else{
+                $('#obj_institucion').addClass('zoomOut animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                  $(this).removeClass('zoomOut animated');
+                  $(this).addClass('hidden');
+                });              
+            }        
+
 
 		});
 	// fin selector
-	
+    $('#form_1').validate({
+        rules: {
+            txt_nom_inst: {
+                required: true
+            },
+            select_tipo_reser_museo: {                
+                required: true
+            }
+        },
+        messages: {
+            txt_nom_inst: {               
+                required: 'Por favor, ingrese nombre de la institución'
+            },
+            select_tipo_reser_museo: {                
+                required: 'Seleccione tipo de reservación'
+            }
+        },
+        highlight: function(element) {
+            $(element).closest('.form-group').addClass('has-error');
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-error');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if(element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        },
+        submitHandler: function (form) {
+            $('#obj_contenedor_museo_matriz').removeClass('col-md-offset-5').addClass('slideInRight animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                $(this).removeClass('slideInRight animated');
+                $('#obj_contenedor_2_museo_matriz').removeClass('hidden').addClass('zoomIn animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                });
+            });
+
+        }
+    });
+    // dar valores iniciales de datepicker
+    var fec=new Date();
+    var acufecha=fec.getDate() + "/" + (fec.getMonth() +1) + "/" + fec.getFullYear();
+    var fec=new Date();
+    $('#datetimepicker1').datetimepicker({
+        viewMode: 'days',
+        format: 'DD/MM/YYYY',
+        minDate: new Date()
+    }).on("dp.change", function(ev) {
+        var fe=$('#txt_fecha_origen').val();
+        buscar_horas(dia_semana(fe),fe);
+    }); 
+    
+    
 });
+
 // cargar obj_img_servicios
 function cargar_menu(){
     $.ajax({
@@ -78,3 +147,55 @@ function cargar_categoria(){
         }
        });	
 }
+// busca la hora que esta disponible en esa fecha
+function buscar_horas(dia,fe){
+    var res=':(';
+    $.ajax({
+        url: "app.php",
+        type: "POST",        
+        data:{museo_buscar_horas:'ok',dia:dia,f:fe},                         
+        success: function(data)
+        {   //console.log(data)   
+            $.gritter.removeAll();
+            $("#tabla_horas tbody").html('');       
+            if (data!=0) {
+                $("#tabla_horas tbody").html(data);
+            }else{              
+                $.gritter.add({
+                    title: 'Estimado Cliente',
+                    text: 'Lo sentimos, no disponemos horarios para realizar reservaciones en esten día',
+                    class_name: 'gritter-info gritter-center',
+                    time:20000
+                });
+            }
+            
+        }                               
+    });
+    //return res;
+}
+//Recibe fecha en formato DD/MM/YYYY  
+function dia_semana(fecha){   
+    fecha=fecha.split('/');  
+    if(fecha.length!=3){
+            return null;  
+    }  
+    //Vector para calcular día de la semana de un año regular.  
+    var regular =[0,3,3,6,1,4,6,2,5,0,3,5];   
+    //Vector para calcular día de la semana de un año bisiesto.  
+    var bisiesto=[0,3,4,0,2,5,0,3,6,1,4,6];   
+    //Vector para hacer la traducción de resultado en día de la semana.  
+    var semana=['DOMINGO', 'LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO'];  
+    //Día especificado en la fecha recibida por parametro.  
+    var dia=fecha[0];  
+    //Módulo acumulado del mes especificado en la fecha recibida por parametro.  
+    var mes=fecha[1]-1;  
+    //Año especificado por la fecha recibida por parametros.  
+    var anno=fecha[2];  
+    //Comparación para saber si el año recibido es bisiesto.  
+    if((anno % 4 == 0) && !(anno % 100 == 0 && anno % 400 != 0))  
+        mes=bisiesto[mes];  
+    else  
+        mes=regular[mes];  
+    //Se retorna el resultado del calculo del día de la semana.  
+    return semana[Math.ceil(Math.ceil(Math.ceil((anno-1)%7)+Math.ceil((Math.floor((anno-1)/4)-Math.floor((3*(Math.floor((anno-1)/100)+1))/4))%7)+mes+dia%7)%7)];  
+} 
--- data/reservacion/app.php
+++ data/reservacion/app.php
@@ -30,8 +30,78 @@ if(!isset($_SESSION))
 		$resultado = $class->consulta("SELECT * FROM seg.categoria_servicio ORDER BY NOM");
 		$i=0;
 		$acu;
+		print'<option value="">Seleccionar</option>';
 		while ($row=$class->fetch_array($resultado)) {
 			print'<option value="'.$row[0].'">'.$row[1].'</option>';		
 		}
 	}
+	if (isset($_POST['museo_buscar_horas'])) {
+		$dia=$_POST['dia'];		
+		//estructura cada fila con un color diferente en class
+		$dis = array("active", "success","warning","danger","info");
+		$resultado = $class->consulta("SELECT horai, horaf, dias, lapso FROM HORARIO_SERVICIOS WHERE ID_SERVICIO='20141211160003548a05d39b5c8' AND STADO ='1';");
+		while ($row=$class->fetch_array($resultado)) {
+			$acu=split(",", $row[2]);
+			for ($i=0; $i < count($acu); $i++) {
+				$dc=strtoupper((String)$acu[$i]);
+				if((string)$dc==$dia){
+					// hora inicial del dia
+					$b=split(":", $row[0]);
+					// finalizacion horario de trabajo
+					$c=split(":", $row[1]);
+					// intervalo de hora
+					$d=split(":", $row[3]);
+					$horaInicial=$b[0].':'.$b[1];
+					$horafinal=$c[0].':'.$c[1];
+					$horalapso=$d[0].':'.$d[1];	
+					//$horafinal=sumar_horas($horafinal,$horalapso);	
+					$horaInicial=restar_horas($horaInicial,$horalapso);	
+					$j=0;
+					// aculumador de horas
+					for ($i=0;strtotime($horaInicial)<strtotime($horafinal);$i++) { 							
+						if ($j==5) {$j=0;};
+						$acumu_horas=sumar_horas($horaInicial,$horalapso);
+						$horaInicial=$acumu_horas;
+						if (strtotime($horaInicial)<strtotime($horafinal)) {	
+							$a=1;
+							print'<tr class="'.$dis[$j].'"><td><label><input type="checkbox" onclick="reconstruir('.$i.')"/><span class="lbl"></span></label></td><td>'.$horaInicial.'</td><td>'.sumar_horas($horaInicial,$horalapso).'</td><td>'.$_POST['f'].'</td><td>'.substr($dia, 0, 3).'...</td></tr>';										
+							$j++;
+						};					
+						
+					}
+
+				}
+			}	
+		}
+	}
+	// requerimiento de funciones 
+	function sumar_horas($hora1,$hora2){	
+		$horaInicial=$hora1;
+		$d=split(':', $hora2);
+		$minutoAnadir=($d[0]*60)+$d[1];
+		$segundos_horaInicial=strtotime($horaInicial);
+		$segundos_minutoAnadir=$minutoAnadir*60;
+		$nuevaHora=date("H:i",$segundos_horaInicial+$segundos_minutoAnadir);
+		return$nuevaHora;
+	}
+	function restar_horas($hora1,$hora2){	
+		$horaInicial=$hora1;
+		$d=split(':', $hora2);
+		$minutoAnadir=($d[0]*60)+$d[1];
+		$segundos_horaInicial=strtotime($horaInicial);
+		$segundos_minutoAnadir=$minutoAnadir*60;
+		$nuevaHora=date("H:i",$segundos_horaInicial-$segundos_minutoAnadir);
+		return$nuevaHora;
+	}
+
+	function buscar_horario($inicio,$fin){
+		$a=0;
+		//print('gb'.$inicio.$fin);
+		$class=new constante();
+		$resultado = $class->consulta("SELECT * FROM RESERVACION_HORARIOS WHERE hinicio='$inicio' and hfin='$fin' AND STADO='0'");
+		while ($row=$class->fetch_array($resultado)) {		
+			$a=1;		
+		}
+		return $a;
+	}	
 ?>
